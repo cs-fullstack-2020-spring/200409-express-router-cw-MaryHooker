15ee48ddf892c2b06eea3a9f472c87f246aebea5
MaryHooker <mc020791@yahoo.com>
2020-04-09 14:38:59 -0500

    api routes created & tested

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ let app = express();
 //variable for port number
 let port = 2112;
 
+//tell express to use JSON middleware
+app.use(express.json());
+
 //import router modules
 let api = require('./routes/api.js');
 let admin = require('./routes/admin.js');
@@ -14,7 +17,6 @@ let admin = require('./routes/admin.js');
 //mount routes
 app.use('/api',api);
 app.use('/admin',admin);
-
 
 //Listen on port
 app.listen(port,()=>{
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,22 @@
 //API route
 
+//Create a router
 let express = require('express');
 let router = express.Router();
 
+//Read in CRUD
 router.get('/:item_id',(req,res)=>{
-    res.send(`You attempted to view item ${req.params.item_id}`)
-})
+    res.send(`You attempted to view item ${req.params.item_id}`);
+});
+
+//Read in CRUD
+router.get('/',(req,res)=>{
+    res.send(`You attempted to view all items`);
+});
+
+//Create in CRUD
+router.post('/',(req,res)=>{
+    res.send(req.body);
+});
 
 module.exports = router;
